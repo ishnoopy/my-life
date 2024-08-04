33c003402307f884d7f3f38119607eef7a739eaf
Aldrin Guasa <aldringuasa@gmail.com>
2024-08-04 21:55:35 +0800

    chore: added post route to list of routes.

--- app.js
+++ app.js
@@ -10,11 +10,13 @@ app.use(express.json());
 // Routes
 const authRoute = require('./routes/auth.routes');
 const userRoute = require('./routes/user.routes');
+const postRoute = require('./routes/post.routes');
 
 // Middlewares
 app.use(loggerMiddleware); // Executes logger middleware for every request before the route handler
 app.use('/api/auth', authRoute);
 app.use('/api/user', userRoute);
+app.use('/api/posts', postRoute);
 
 // Error handling middleware for invalid routes
 app.use((req, res) => {
