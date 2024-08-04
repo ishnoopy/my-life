437f2d8227fa194724176769a33928eef8be20e5
Aldrin Guasa <aldringuasa@gmail.com>
2024-08-04 21:53:06 +0800

    chore: added post model.

---
+++ models/post.model.js
@@ -0,0 +1,44 @@
+const sequelize = require('../db/connection');
+const { DataTypes } = require('sequelize');
+
+const Post = sequelize.define('Post', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  user_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'users',
+      key: 'id'
+    }
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  content: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  visibility: {
+    type: DataTypes.ENUM(0, 1),
+    allowNull: false,
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    field: 'created_at',
+    defaultValue: DataTypes.NOW
+  },
+  updatedAt: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    field: 'updated_at',
+    defaultValue: DataTypes.NOW
+  }
+})
+
+module.exports = Post;
