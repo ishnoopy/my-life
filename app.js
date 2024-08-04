42509ab7eb48ce86d906a3651cfa813adf2b7cdd
Aldrin Guasa <aldringuasa@gmail.com>
2024-08-04 20:50:48 +0800

    chore: add express server entry point including basic logging and auth, user routes.

---
+++ app.js
@@ -0,0 +1,27 @@
+const { loggerMiddleware } = require('./middlewares/logger');
+const { port } = require('./config/config');
+
+const express = require('express');
+const app = express();
+
+
+app.use(express.json());
+
+// Routes
+const authRoute = require('./routes/auth.routes');
+const userRoute = require('./routes/user.routes');
+
+// Middlewares
+app.use(loggerMiddleware); // Executes logger middleware for every request before the route handler
+app.use('/api/auth', authRoute);
+app.use('/api/user', userRoute);
+
+// Error handling middleware for invalid routes
+app.use((req, res) => {
+  res.status(404).json({ message: 'Invalid route' });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+})
+
