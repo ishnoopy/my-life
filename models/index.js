a154b3d0da2822b67d6eacb2839bf77fba94d940
Aldrin Guasa <aldringuasa@gmail.com>
2024-08-04 21:53:15 +0800

    chore: export post model.

--- models/index.js
+++ models/index.js
@@ -1,11 +1,14 @@
 const sequelize = require('../db/connection');
 const UserModel = require('./user.model');
 const ProfileModel = require('./profile.model');
+const PostModel = require('./post.model');
+
 
 sequelize.sync();
 
 module.exports = {
   UserModel,
   ProfileModel,
+  PostModel,
   sequelize
 }
